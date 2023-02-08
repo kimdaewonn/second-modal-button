568e7826eddd21a26d6607ff19bf607b8a5290ab
kimdaewonn <eodnjs9605@gmail.com>
2023-02-08 23:53:18 +0900

    go

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Buttonn from "./Buttonn";
-import Button from "@mui/material/Button";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Main = () => {
